2ab272319b007f14ec0b05179b8d4e809a59e694
thony-ui <anthonyhermanto1@gmail.com>
2024-09-16 19:39:00 +0800

    Test pr wanna check if everything is wroking

--- src/main/java/seedu/address/logic/Messages.java
+++ src/main/java/seedu/address/logic/Messages.java
@@ -12,6 +12,8 @@ import seedu.address.model.person.Person;
  */
 public class Messages {
 
+    // messages
+
     public static final String MESSAGE_UNKNOWN_COMMAND = "Unknown command";
     public static final String MESSAGE_INVALID_COMMAND_FORMAT = "Invalid command format! \n%1$s";
     public static final String MESSAGE_INVALID_PERSON_DISPLAYED_INDEX = "The person index provided is invalid";
